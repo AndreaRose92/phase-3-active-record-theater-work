64f684eadf1e177111019ed632d5e7f6810fa564
Kelsey Weeks <kelsey@kelseyweeks.com>
2022-08-16 09:06:23 -0500

    Role methods

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,29 @@ class Role < ActiveRecord::Base
 
     has_many :auditions
 
+    def auditions
+        Audition.where role_id: self.id
+    end
 
+   
+    def actors
+        self.auditions.pluck :actor
+    end
 
+    
+    def location
+        self.auditions.pluck :location
+    end
 
+    
+    def lead
+        actors = self.auditions.where(hired: true)
+        actors.exists? ? actors.first : "no actor has been hired for this role"
+    end
+
+    
+    def understudy
+        actors = self.auditions.where(hired: true)
+        actors.length >= 2 ? actors.second : "'no actor has been hired for understudy for this role"
+    end
 end
